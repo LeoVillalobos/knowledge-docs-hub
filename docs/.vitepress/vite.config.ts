c39d7f6aa8b0755b2325ed958e3f7ad3d423d8a4
Leonardo Villalobos <63274441+LeoVillalobos@users.noreply.github.com>
2025-04-12 13:48:05 -0600

    styles: none,

--- docs/.vitepress/vite.config.ts
+++ docs/.vitepress/vite.config.ts
@@ -1,6 +1,17 @@
+// .vitepress/vite.config.ts
 import { defineConfig } from 'vite'
 import vuetify from 'vite-plugin-vuetify'
+// import path from 'path'
 
 export default defineConfig({
-  plugins: [vuetify()],
+  plugins: [
+    vuetify({
+      styles: 'none', // 👈 Desactiva los estilos automáticos
+    }),
+  ],
+  // resolve: {
+  //   alias: {
+  //     '@components': path.resolve(__dirname, '../src/components'),
+  //   },
+  // },
 })
