227718b4f91dd60a479e1d152d591f166b2d6b8e
Leonardo Villalobos <63274441+LeoVillalobos@users.noreply.github.com>
2025-04-21 09:21:10 -0600

    change name

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -11,7 +11,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
-import './index.css'
+import './style.css'
 
 const vuetify = createVuetify({
   components,
