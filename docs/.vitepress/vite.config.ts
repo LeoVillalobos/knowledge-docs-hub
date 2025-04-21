4f56e9738b9c843e69b32fe9fe7d70f97352e1b8
Leonardo Villalobos <63274441+LeoVillalobos@users.noreply.github.com>
2025-04-21 10:10:55 -0600

    test publish

--- docs/.vitepress/vite.config.ts
+++ docs/.vitepress/vite.config.ts
@@ -4,10 +4,20 @@ import vuetify from 'vite-plugin-vuetify'
 export default defineConfig({
   vite: {
     plugins: [
-      vuetify({ autoImport: true }), // Habilita auto-importación
+      vuetify({
+        autoImport: true,
+        styles: { configFile: 'docs/.vitepress/theme/settings.scss' } // <-- Esto faltaba
+      }),
     ],
     ssr: {
-      noExternal: ['vuetify'], // Necesario para SSR
+      noExternal: ['vuetify']
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "vuetify/styles";' // <-- Y esto es necesario
+        }
+      }
     }
   },
   // ... resto de tu configuración
